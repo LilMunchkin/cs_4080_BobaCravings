693a71c200e0c1716de8cc758b546ef72f5d5267
KossBoii <long.truong0320@gmail.com>
2020-05-04 11:11:22 -0700

    test stack + recompile

--- src/test_in_c++/main.cpp
+++ src/test_in_c++/main.cpp
@@ -1,26 +1,68 @@
 #include <iostream>
 #include "stack.h"
+#include <stack>
+#include "timer.h" 
 
 using namespace std;
 
+static const int ELEMENTS = 1000000;
+
+void testStandardStack(Timer& t);
+void testImplementStack(Timer& t);
+
 int main(){
     // test Stack:
-    Stack<int> stack;
-    for(int i=0; i <10; i++){
-        stack.push(i);
-    }
-    cout<<"Stack: "<<stack<<endl;
+    Timer t;
 
-    Stack<int> stack2(stack);
-    cout<<"Stack 2: "<<stack2<<endl;
-
-    while(!stack2.empty())
-        cout<<"{"<<stack2.pop()<<"} "<<stack2<<endl;
-
-    cout<<"Assigning stack back to stack2: "<<endl;
-    stack2 = stack;
-    cout<<"Stack: "<<stack<<endl;
-    cout<<"Stack2: "<<stack2<<endl;
+    testImplementStack(t);
+    cout<<"\n\n\n\n----------------------------------------------------\n";
+    testStandardStack(t);
 
     return 0;
+}
+
+void testImplementStack(Timer& t){
+    t.start();
+    Stack<int> stack;
+    t.stop();
+    cout<<"Creating stack takes: "<<t.duration()<<" seconds\n";
+
+    t.start();
+    for(int i=0; i <ELEMENTS; i++){
+        stack.push(i);
+    }
+    t.stop();
+    // cout<<"Stack: "<<stack<<endl;
+    cout<<"Pushing "<<ELEMENTS<<" elements into stack take: "<<t.duration()<<" seconds\n";
+
+
+    t.start();
+    for(int i=0; i<ELEMENTS;i++){
+        stack.pop();
+    }
+    t.stop();
+    cout<<"Popping "<<ELEMENTS<<" elements from the stack take: "<<t.duration()<<" seconds\n";
+}
+
+void testStandardStack(Timer& t){
+    t.start();
+    stack<int> s;
+    t.stop();
+    cout<<"Creating stack takes: "<<t.duration()<<" seconds\n";
+
+    t.start();
+    for(int i=0; i <ELEMENTS; i++){
+        s.push(i);
+    }
+    t.stop();
+    // cout<<"Stack: "<<stack<<endl;
+    cout<<"Pushing "<<ELEMENTS<<" elements into stack take: "<<t.duration()<<" seconds\n";
+
+
+    t.start();
+    for(int i=0; i<ELEMENTS;i++){
+        s.pop();
+    }
+    t.stop();
+    cout<<"Popping "<<ELEMENTS<<" elements from the stack take: "<<t.duration()<<" seconds\n";
 }
