812ccbbb5f43f73233a8af49fb4b0ab9dfc0f82d
jho012299 <jho012299@gmail.com>
2020-05-05 20:16:41 -0700

    Added one method

--- src/test_in_python/Stack.py
+++ src/test_in_python/Stack.py
@@ -1,4 +1,5 @@
 from collections import deque
+from queue import LifoQueue
 import time
 
 def listMethod():
@@ -19,9 +20,21 @@ def dequeMethod():
     for j in range(1000000):
         stack.pop()
 
+def lifoQueueMethod():
+    stack = LifoQueue(maxsize = 1000000)
+
+    for i in range(1000000):
+        stack.put(i)
+
+    for j in range(1000000):
+        stack.get()
+
 startTime = time.time()
 listMethod()
 print("Execution Time for List Based Stack: ", time.time() - startTime, " seconds")
 startTime = time.time()
 dequeMethod()
 print("Execution Time for Deque Based Stack: ", time.time() - startTime, " seconds")
+startTime = time.time()
+lifoQueueMethod()
+print("Execution Time for Lifo Queue Based Stack: ", time.time() - startTime, " seconds")
